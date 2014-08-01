6a9b8e928af16140fa7925b1fa45c884a12fcb8e
Bram van der Veen <96aa48@gmail.com>
2014-08-01 12:59:19 +0200

    Fixed mplayer cutting out

--- index.js
+++ index.js
@@ -3,39 +3,38 @@ var http = require('http');
 var fs = require('fs');
 var mplayer = require('child_process').spawn;
 var readline = require('readline');
+var nullstream = require('dev-null-stream');
 
 var input = readline.createInterface({input:process.stdin, output:process.stdout});
 
 input.question('What song are you looking for?\n', function (answer) {
-input.close();
+	input.close();
+	var query = answer;
+	var nresults = 20;
 
+	//console.log(query);
 
+	http.get('http://tinysong.com/s/' + query + '?format=json&limit=' + nresults + '&key=0131065fac026c65c87e3658dfa66b88', function (res) {
+		var body = '';
+		res.on('data', function (data) {
+			body += data;
+		});	
+		res.on('end', function () {
+			show(body);
 
-var query = answer;
-var nresults = 20;
+		});
 
-console.log(query);
-
-http.get('http://tinysong.com/s/' + query + '?format=json&limit=' + nresults + '&key=0131065fac026c65c87e3658dfa66b88', function (res) {
-var body = '';
-res.on('data', function (data) {
-	body += data;
-});
-res.on('end', function () {
-	show(body);
-
-});
-
-});
+	});
 
 });
 
 function show(data) {
-data = JSON.parse(data);
+	data = JSON.parse(data);
 
-for (i = 0; i < data.length; i++) {
-	console.log('[' + i + '] ' + data[i].SongName + ' - ' + data[i].ArtistName);
-}
+	for (i = 0; i < data.length; i++) {
+		console.log('[' + i + '] ' + data[i].SongName + ' - ' + data[i].ArtistName);
+	}
+	
 	var rl = readline.createInterface({input:process.stdin, output:process.stdout})
 
 	rl.question('What song do you want to download?', function (answer) {
@@ -43,34 +42,34 @@ for (i = 0; i < data.length; i++) {
 		download(data[answer]);	
 		rl.close();
 	});
-
-
-
 }
 
 function download (songInfo) {
 
-GS.Grooveshark.getStreamingUrl(songInfo.SongID, function (err, streamUrl) { 
-var filename = __dirname + '/' + songInfo.SongName + '.mp3';
-http.get(streamUrl, function(res) {
-	res.on('data', function (data){
-		if (fs.existsSync(filename)) {
-			fs.appendFileSync(filename, data);
-		}
-		else {
-			fs.writeFileSync(filename, data);
-		}
-	});
+	GS.Grooveshark.getStreamingUrl(songInfo.SongID, function (err, streamUrl) { 
+		var filename = __dirname + '/' + songInfo.SongName + '.mp3';
+		http.get(streamUrl, function(res) {
+			res.on('data', function (data){
+				if (fs.existsSync(filename)) {
+				fs.appendFileSync(filename, data);
+				}
+				else {
+				fs.writeFileSync(filename, data);
+				}
+			});
 
-	res.on('end', function () {
-		var player = mplayer('mplayer', ['-ao','alsa', filename]);
-		player.on('error', function (data) {
-			console.log(data);
-		});
-	});
+			res.on('end', function () {
+				var player = mplayer('mplayer', ['-ao','alsa', filename]);
+				player.on('error', function (data) {
+					console.log(data);
+				});
 
-});	
+				player.stdout.pipe(new nullstream());
+				process.stdin.pipe(player.stdin);
+			});
+
+		});	
 	
-});
+	});
 }
 
