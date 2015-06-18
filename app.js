60bee0dedd0997fa629037cdf8edc0ea22f8c888
Vincent Lark <vincent.lark@gmail.com>
2015-06-18 12:08:13 +0200

    Find home directory in platform agnostic way

--- app.js
+++ app.js
@@ -10,12 +10,16 @@ var mplayer = require('child_process').spawn;
 var readline = require('readline').createInterface({input : process.stdin, output : process.stdout});
 
 var args = []; for (i=0;i<process.argv.join(' ').split(' -').length;i++) {if (i!=0) args[i-1] = process.argv.join(' ').split(' -')[i]}; if (args.length == 0) help();
-if (!fs.existsSync('/home/' + process.env['USER'] + '/Music')) {fs.mkdirSync('/home/' + process.env['USER'] + '/Music', 0775)}
+if (!fs.existsSync(getUserHome() + '/Music')) {fs.mkdirSync(getUserHome() + '/Music', 0775)}
 _.each(args, function (item) {
 	if (item == '-help' || item == 'h') { help();}
 	else if (item == 'o' || item == '-offline') {return offline(); process.exit(0);}
 	else {return lookup(item.substr(2,item.length)); process.exit(0);}
 });
+
+function getUserHome() {
+  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
 
 function help() {
 	process.stdout.write('yplayer v0.0.5, by: Bram \"#96AA48\" van der Veen\n\n');
@@ -30,9 +34,9 @@ function help() {
 }
 
 function offline() {
-	var files = []; for (i=0;i<fs.readdirSync('/home/' + process.env['USER'] + '/Music').length;i++) {files[i] = fs.readdirSync('/home/' + process.env['USER'] + '/Music')[i];};
+	var files = []; for (i=0;i<fs.readdirSync(getUserHome() + '/Music').length;i++) {files[i] = fs.readdirSync(getUserHome() + '/Music')[i];};
 	for (i=0;i<files.length;i++) {process.stdout.write('['.cyan + i + '] '.cyan + (files[i].split('.mp3')[0]).bold + '\n'); if (i==files.length - 1) process.stdout.write('\n');};
-	readline.question('What song do you want to play? #', function (input) {if (parseInt(input) != NaN) play('/home/' + process.env['USER'] + '/Music/' + files[input]);});
+	readline.question('What song do you want to play? #', function (input) {if (parseInt(input) != NaN) play(getUserHome() + '/Music/' + files[input]);});
 }
 
 function lookup(query) {
@@ -84,10 +88,10 @@ function checkApi(callback) {
 
 function getLocation(type, data) {
 	if (type == 'download') {
-		return '/home/' + process.env['USER'] + '/Music/' + data + '.mp3';
+		return getUserHome() + '/Music/' + data + '.mp3';
 	}
 	else if (type == 'api') {
-		return '/home/' + process.env['USER'] + '/.yplayerrc';
+		return getUserHome() + '/.yplayerrc';
 	}
 }
 
